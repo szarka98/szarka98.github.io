44c7c02e6f76a178c8e68da57fb9d72d22a5a990
szarka98 <szarka98@gmail.com>
2019-01-05 19:24:21 +0100

    Refact: 3dim array 3.0

--- motion.js
+++ motion.js
@@ -9,24 +9,35 @@ canvas.width = ww;
 var size = 100;
 var leftX = ww / 2 - 2.5 * size;
 
-var v0 = [];
-var v = [];
-var dv = [];
-for (var i = 0; i < 5; i++) {
-  v0[i] = [[leftX + size * i, 100], [leftX + size * i, 200], [leftX + size * i, 300]];
-  v[i] = [[v0[i][0][0], v0[i][0][1]], [v0[i][1][0], v0[i][1][1]], [v0[i][2][0], v0[i][2][1]]];
-  dv[i] = [
-    [Math.random() * 2 + -1, Math.random() * 2 + -1],
-    [Math.random() * 2 + -1, Math.random() * 2 + -1],
-    [Math.random() * 2 + -1, Math.random() * 2 + -1]
-  ];
-}
-
 var speed = 12;
-var elemName = [["St", "At", "E", " ", "Of"], ["Ja", "Va", "Sc", "Ri", "Pt"], [" ", " ", "20", "18", " "]];
+
+var elemName = [
+  ["St", "At", "E", " ", "Of"],
+  ["Ja", "Va", "Sc", "Ri", "Pt"],
+  [" ", " ", "20", "18", " "]
+];
 var elemNameColor = ["#e1e1e1", "#41c7c7", "#fe6a6a"];
 var elemNum = [[0, 1, 2, 0, 3], [4, 5, 6, 7, 8], [0, 0, 9, 10, 0]];
+// hide factor
 var elemBoole = [[1, 1, 1, 0, 1], [1, 1, 1, 1, 1], [0, 0, 1, 1, 0]];
+// starting point
+var v0 = [];
+// current coordinates
+var v = [];
+// speed vector
+var dv = [];
+
+for (var i = 0; i < 5; i++) {
+  // TODO: beautify multidim array declaration
+  v0[i] = [];
+  v[i] = [];
+  dv[i] = [];
+  for (var j = 0; j < 3; j++) {
+    v0[i][j] = [leftX + size * i, 100 * (1 + j)];
+    v[i][j] = [v0[i][j][0], v0[i][j][1]];
+    dv[i][j] = [Math.random() * 2 + -1, Math.random() * 2 + -1];
+  }
+}
 
 var startBtn = document.getElementById("start-button");
 var magnet = false;
@@ -44,202 +55,74 @@ function magnetOff() {
 }
 
 function drawElem(i) {
-  if (elemBoole[2][i]) {
-    ctx.beginPath();
-    // box
-    ctx.rect(v[i][2][0], v[i][2][1], size, size);
-    ctx.fillStyle = "#212424";
-    ctx.fillRect(v[i][2][0], v[i][2][1], size, size);
-    ctx.strokeStyle = "#646a6a";
-    ctx.strokeWidth = 1;
-    ctx.stroke();
-    // elem
-    ctx.font = "600 2.4rem IBM Plex Mono";
-    ctx.textAlign = "center";
-    ctx.fillStyle = elemNameColor[2];
-    ctx.fillText(elemName[2][i], v[i][2][0] + size / 2, v[i][2][1] + size * 0.6);
-    // number
-    ctx.font = "300 13px IBM Plex Mono";
-    ctx.fillStyle = "#e1e1e1";
-    ctx.fillText(
-      elemNum[2][i],
-      v[i][2][0] + size * 0.14,
-      v[i][2][1] + size * 0.19
-    );
-    ctx.closePath();
-  }
-  if (elemBoole[1][i]) {
-    ctx.beginPath();
-    // box
-    ctx.rect(v[i][1][0], v[i][1][1], size, size);
-    ctx.fillStyle = "#212424";
-    ctx.fillRect(v[i][1][0], v[i][1][1], size, size);
-    ctx.strokeStyle = "#646a6a";
-    ctx.strokeWidth = 1;
-    ctx.stroke();
-    // elem
-    ctx.font = "600 2.4rem IBM Plex Mono";
-    ctx.textAlign = "center";
-    ctx.fillStyle = elemNameColor[1];
-    ctx.fillText(elemName[1][i], v[i][1][0] + size / 2, v[i][1][1] + size * 0.6);
-    // number
-    ctx.font = "300 13px IBM Plex Mono";
-    ctx.fillStyle = "#e1e1e1";
-    ctx.fillText(
-      elemNum[1][i],
-      v[i][1][0] + size * 0.14,
-      v[i][1][1] + size * 0.19
-    );
-    ctx.closePath();
-  }
-  if (elemBoole[0][i]) {
-    ctx.beginPath();
-    // box
-    ctx.rect(v[i][0][0], v[i][0][1], size, size);
-    ctx.fillStyle = "#212424";
-    ctx.fillRect(v[i][0][0], v[i][0][1], size, size);
-    ctx.strokeStyle = "#646a6a";
-    ctx.strokeWidth = 1;
-    ctx.stroke();
-    // elem
-    ctx.font = "600 2.4rem IBM Plex Mono";
-    ctx.textAlign = "center";
-    ctx.fillStyle = elemNameColor[0];
-    ctx.fillText(elemName[0][i], v[i][0][0] + size / 2, v[i][0][1] + size * 0.6);
-    // number
-    ctx.font = "300 13px IBM Plex Mono";
-    ctx.fillStyle = "#e1e1e1";
-    ctx.fillText(
-      elemNum[0][i],
-      v[i][0][0] + size * 0.14,
-      v[i][0][1] + size * 0.19
-    );
-    ctx.closePath();
+  for (var j = 0; j < 3; j++) {
+    if (elemBoole[j][i]) {
+      ctx.beginPath();
+      // box
+      ctx.rect(v[i][j][0], v[i][j][1], size, size);
+      ctx.fillStyle = "#212424";
+      ctx.fillRect(v[i][j][0], v[i][j][1], size, size);
+      ctx.strokeStyle = "#646a6a";
+      ctx.strokeWidth = 1;
+      ctx.stroke();
+      // elem
+      ctx.font = "600 2.4rem IBM Plex Mono";
+      ctx.textAlign = "center";
+      ctx.fillStyle = elemNameColor[j];
+      ctx.fillText(
+        elemName[j][i],
+        v[i][j][0] + size / 2,
+        v[i][j][1] + size * 0.6
+      );
+      // number
+      ctx.font = "300 13px IBM Plex Mono";
+      ctx.fillStyle = "#e1e1e1";
+      ctx.fillText(
+        elemNum[j][i],
+        v[i][j][0] + size * 0.14,
+        v[i][j][1] + size * 0.19
+      );
+      ctx.closePath();
+    }
   }
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   for (var i = 0; i < 5; i++) {
-    drawElem(i);
-    if (!magnet) {
-      
-      if (
-        v[i][2][0] + dv[i][2][0] * speed > ww - size ||
-        v[i][2][0] + dv[i][2][0] * speed < 0
-      ) {
-        speed = 3;
-        dv[i][2][0] = -dv[i][2][0];
-      }
-      if (
-        v[i][2][1] + dv[i][2][1] * speed > wh - size ||
-        v[i][2][1] + dv[i][2][1] * speed < 0
-      ) {
-        speed = 3;
-        dv[i][2][1] = -dv[i][2][1];
-      }
+    for (var j = 0; j < 3; j++) {
+      drawElem(i);
 
+      if (!magnet) {
+        if (
+          v[i][j][0] + dv[i][j][0] * speed > ww - size ||
+          v[i][j][0] + dv[i][j][0] * speed < 0
+        ) {
+          speed = 3;
+          dv[i][j][0] = -dv[i][j][0];
+        }
+        if (
+          v[i][j][1] + dv[i][j][1] * speed > wh - size ||
+          v[i][j][1] + dv[i][j][1] * speed < 0
+        ) {
+          speed = 3;
+          dv[i][j][1] = -dv[i][j][1];
+        }
 
-      if (
-        v[i][1][0] + dv[i][1][0] * speed > ww - size ||
-        v[i][1][0] + dv[i][1][0] * speed < 0
-      ) {
-        speed = 3;
-        dv[i][1][0] = -dv[i][1][0];
-      }
-      if (
-        v[i][1][1] + dv[i][1][1] * speed > wh - size ||
-        v[i][1][1] + dv[i][1][1] * speed < 0
-      ) {
-        speed = 3;
-        dv[i][1][1] = -dv[i][1][1];
-      }
-
-      if (
-        v[i][0][0] + dv[i][0][0] * speed > ww - size ||
-        v[i][0][0] + dv[i][0][0] * speed < 0
-      ) {
-        speed = 3;
-        dv[i][0][0] = -dv[i][0][0];
-      }
-      if (
-        v[i][0][1] + dv[i][0][1] * speed > wh - size ||
-        v[i][0][1] + dv[i][0][1] * speed < 0
-      ) {
-        speed = 3;
-        dv[i][0][1] = -dv[i][0][1];
-      }
-
-      v[i][2][0] += dv[i][2][0] * speed;
-      v[i][2][1] += dv[i][2][1] * speed;
-
-      v[i][1][0] += dv[i][1][0] * speed;
-      v[i][1][1] += dv[i][1][1] * speed;
-      
-      v[i][0][0] += dv[i][0][0] * speed;
-      v[i][0][1] += dv[i][0][1] * speed;
-    
-    } else {
-      speed = 14;
-
-      if (
-        Math.abs(v0[i][2][0] - v[i][2][0]) > speed ||
-        Math.abs(v0[i][2][1] - v[i][2][1]) > speed
-      ) {
-        v[i][2][0] =
-          v[i][2][0] +
-          (speed * (v0[i][2][0] - v[i][2][0])) /
-            (Math.abs(v0[i][2][0] - v[i][2][0]) +
-              Math.abs(v0[i][2][1] - v[i][2][1]));
-        v[i][2][1] =
-          v[i][2][1] +
-          (speed * (v0[i][2][1] - v[i][2][1])) /
-            (Math.abs(v0[i][2][0] - v[i][2][0]) +
-              Math.abs(v0[i][2][1] - v[i][2][1]));
+        v[i][j][0] += dv[i][j][0] * speed;
+        v[i][j][1] += dv[i][j][1] * speed;
       } else {
-        v[i][2][0] = v0[i][2][0];
-        v[i][2][1] = v0[i][2][1];
+        if (
+          Math.abs(v0[i][j][0] - v[i][j][0]) > 3 ||
+          Math.abs(v0[i][j][1] - v[i][j][1]) > 3
+        ) {
+          v[i][j][0] += (v0[i][j][0] - v[i][j][0]) / 8;
+          v[i][j][1] += (v0[i][j][1] - v[i][j][1]) / 8;
+        } else {
+          v[i][j][0] = v0[i][j][0];
+          v[i][j][1] = v0[i][j][1];
+        }
       }
-
-      if (
-        Math.abs(v0[i][1][0] - v[i][1][0]) > speed ||
-        Math.abs(v0[i][1][1] - v[i][1][1]) > speed
-      ) {
-        v[i][1][0] =
-          v[i][1][0] +
-          (speed * (v0[i][1][0] - v[i][1][0])) /
-            (Math.abs(v0[i][1][0] - v[i][1][0]) +
-              Math.abs(v0[i][1][1] - v[i][1][1]));
-        v[i][1][1] =
-          v[i][1][1] +
-          (speed * (v0[i][1][1] - v[i][1][1])) /
-            (Math.abs(v0[i][1][0] - v[i][1][0]) +
-              Math.abs(v0[i][1][1] - v[i][1][1]));
-      } else {
-        v[i][1][0] = v0[i][1][0];
-        v[i][1][1] = v0[i][1][1];
-      }
-
-      if (
-        Math.abs(v0[i][0][0] - v[i][0][0]) > speed ||
-        Math.abs(v0[i][0][1] - v[i][0][1]) > speed
-      ) {
-        v[i][0][0] =
-          v[i][0][0] +
-          (speed * (v0[i][0][0] - v[i][0][0])) /
-            (Math.abs(v0[i][0][0] - v[i][0][0]) +
-              Math.abs(v0[i][0][1] - v[i][0][1]));
-        v[i][0][1] =
-          v[i][0][1] +
-          (speed * (v0[i][0][1] - v[i][0][1])) /
-            (Math.abs(v0[i][0][0] - v[i][0][0]) +
-              Math.abs(v0[i][0][1] - v[i][0][1]));
-      } else {
-        v[i][0][0] = v0[i][0][0];
-        v[i][0][1] = v0[i][0][1];
-      }
-
     }
   }
 }
